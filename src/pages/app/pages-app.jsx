d11253fe8792aab6f4d19cdc4c9334661e2f412e
Le Anh Quan <quanla2003@gmail.com>
2018-07-05 09:15:30 +0700

    Init generator

---
+++ src/pages/app/pages-app.jsx
@@ -0,0 +1,19 @@
+import classnames from "classnames";
+import {FComponent} from "../common/f-component";
+
+export class PagesApp extends FComponent {
+
+
+    constructor(props, context) {
+        super(props, context);
+
+    }
+
+    render() {
+        return (
+            <div className="pages-app">
+                PagesApp
+            </div>
+        );
+    }
+}
---
+++ src/pages/loader.jsx
@@ -0,0 +1,5 @@
+import {PagesApp} from "./app/pages-app";
+
+ReactDOM.render((
+    <PagesApp style={{height: "100%"}} />
+), document.getElementById("app-container"));
