f2cce1d6e304dc2b1aed17d0630c6a46adcaed2c
Le Anh Quan <quanla2003@gmail.com>
2018-07-06 10:42:52 +0700

    Docs

--- src/pages/app/routes/docs/docs-route.jsx
+++ src/pages/app/routes/docs/docs-route.jsx
@@ -29,7 +29,7 @@ export class DocsRoute extends FComponent {
     render() {
         const {content} = this.state;
         const {location} = this.props;
-        // content && console.log(content);
+
         return (
             <DocsLayout
                 leftNav={
