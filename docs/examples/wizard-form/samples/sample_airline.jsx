db7573b36f064d82a294bf8601d732f43cb4056e
Le Anh Quan <quanla2003@gmail.com>
2018-08-02 17:52:02 +0700

    deploy

--- docs/examples/wizard-form/samples/sample_airline.jsx
+++ docs/examples/wizard-form/samples/sample_airline.jsx
@@ -1,10 +1,12 @@
-import cln from "classnames";
 import React from "react";
 import {createForm} from "bee-form-react";
-import {required, notEqualsPath} from "bee-form-validators";
+import {required, notEqualsPath, equals} from "bee-form-validators";
 import {intNumber} from "bee-form-tunnels";
 import {Step1Flight} from "./step1-flight";
 import {Step2Guests} from "./step2-guests";
+import {Step3Time} from "./step3-time";
+import {Step4SeatPositions} from "./step4-seat-positions";
+import {Step5Checkout} from "./step5-checkout";
 
 class SampleAirline extends React.Component {
 
@@ -13,6 +15,11 @@ class SampleAirline extends React.Component {
 
         this.state = {
             stepIndex: 0,
+        };
+
+
+        let initData = {
+            guests: [{}],
         };
 
         this.form = createForm({
@@ -24,9 +31,15 @@ class SampleAirline extends React.Component {
                 validators: [required],
                 tunnel: [intNumber],
             },
-        }, {
-            guests: [{}],
-        });
+
+            "time": [required],
+
+            "seat_positions": [
+                // Validate if there are a seat for each guests
+                {name: "length", validate: (positions, data) => positions ? positions.length === data.guests.length : false}
+            ],
+            "checkout.ok": [equals(true)],
+        }, initData);
 
         this.form.onChange(() => this.forceUpdate());
 
@@ -54,6 +67,33 @@ class SampleAirline extends React.Component {
                     <Step2Guests
                         fv={fv.scope("guests")}
                         onNext={next}
+                    />
+                ),
+            },
+            {
+                label: "Time",
+                render: () => (
+                    <Step3Time
+                        fv={fv.scope("time")}
+                        onNext={next}
+                    />
+                ),
+            },
+            {
+                label: "Seat positions",
+                render: () => (
+                    <Step4SeatPositions
+                        fv={fv.scope("seat_positions")}
+                        onNext={next}
+                    />
+                ),
+            },
+            {
+                label: "Seat positions",
+                render: () => (
+                    <Step5Checkout
+                        fv={fv.scope("checkout")}
+                        onFinish={() => alert(`Here is your data:\n\n${JSON.stringify(fv.getValue())}\n\nJust post to server and done`)}
                     />
                 ),
             },
